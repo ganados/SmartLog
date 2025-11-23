98f4dc4a857167e99091368d920f36278fe86fb6
pjarosz <przemyslaw.jarosz@partner.bmwgroup.com>
2025-11-23 19:50:41 +0100

    Initial project structure and gitignore

---
+++ backend/src/test/java/com/smartlog/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartlog.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
